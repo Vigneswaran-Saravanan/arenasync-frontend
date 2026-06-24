1ca366b79969906372227745d39ceb529c24170d
Vigneswaran-Saravanan <s.vigneswaran007@gmail.com>
2026-06-24 02:48:18 -0400

    update base url to read from environment variable for production deployment

--- arenasync/src/services/api.js
+++ arenasync/src/services/api.js
@@ -3,25 +3,27 @@ import axios from 'axios'
 
 // The base URL of your backend
 // API calls starts with this URL
-const BASE_URL = 'http://localhost:5000/api'
+const BASE_URL = import.meta.env.VITE_API_URL
+    ? import.meta.env.VITE_API_URL + '/api'
+    : 'http://localhost:5000/api'
 
 // Create an axios with base URL
 const api = axios.create({
     baseURL: BASE_URL,
     headers: {
-        'Content-Type' : 'application/json',
+        'Content-Type': 'application/json',
     },
 })
 
 // Auth API calls
 
 // Register a new user
-export function registerUser(userData){
+export function registerUser(userData) {
     return api.post('/auth/register', userData)
 }
 
 // Login an existing user
-export function loginUser(credentials){
+export function loginUser(credentials) {
     return api.post('/auth/login', credentials)
 }
 
